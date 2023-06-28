605b2834dae95ceb66d6d42e112958ce70093651
Priyanka Solanki <priyankasolanki.1820@gmail.com>
2023-06-28 18:38:37 +0530

    registration-form

---
+++ src/view/routernav/RouterNav.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import HomeScreen from "../screen/HomeScreen";
+import RegisterScreen from "../screen/RegisterScreen";
+import LoginScreen from "../screen/LoginScreen";
+
+export default function RouterNav(){
+    return(
+      <>
+      <Routes>
+        <Route path="/" Component={HomeScreen}/>
+        <Route path="/register" Component={RegisterScreen}/>
+        <Route path="/login" Component={LoginScreen}/>
+        <Route path="*" Component={HomeScreen}/>
+      </Routes>
+      </>
+    )
+  }
