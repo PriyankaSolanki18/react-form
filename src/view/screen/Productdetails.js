86e3149a21c44847a53c94021a2485acaab03500
Priyanka Solanki <priyankasolanki.1820@gmail.com>
2023-07-03 15:38:43 +0530

    updatefirst

---
+++ src/view/screen/Productdetails.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+
+export default function Productdetails(){
+    const nav = useNavigate()
+    const loc =useLocation()
+    const [data,setData] = useState(loc.state)
+
+console.log(data);  
+  return(
+        <>
+        <h1 className="prod-name">Product Details</h1>
+        <div className="pdetails">
+            <div className="pdetails-img">
+                <img src={data.image} />
+            </div>    
+            <div className="pdetails-text">
+                <p>{data.productname}</p>
+                <p>Rs.{data.rate}/-</p>
+                <button>
+                <span>-</span>
+                </button>
+                <span> 1 </span>
+                <button>
+                <span>+</span>
+                </button>
+            </div>    
+
+            <div className="pdetails-pay">
+                <p>Total pay: </p>
+                <button onClick={()=>nav('/cart',{state:""})}>Add To Cart</button>
+                <button onClick={()=>nav('/payment',{state:""})}>Buy Now</button>
+            </div>    
+        </div>
+        </>
+    )
+}
